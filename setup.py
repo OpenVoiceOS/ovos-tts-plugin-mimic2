ab9fd297c0d0019d5f78f78ec8df128e00a01f10
jarbasal <jarbasai@mailfence.com>
2020-12-03 04:37:14 +0000

    pip package

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
-from os.path import join, abspath, dirname
 from setuptools import setup
-
-with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
-    requirements = f.readlines()
 
 PLUGIN_ENTRY_POINT = 'catotron_tts_plug = ' \
                      'mycroft_tts_plugin_catotron:CatotronTTSPlugin'
 setup(
     name='mycroft-tts-plugin-catotron',
-    version='0.1',
+    version='0.1.1',
     description='A catalan tacotron based tts plugin for mycroft',
-    url='https://github.com/OpenJarbas/mycroft-tts-plugin-catotron',
+    url='https://github.com/JarbasIberianLanguageResources/mycroft-tts-plugin-catotron',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     packages=['mycroft_tts_plugin_catotron'],
-    install_requires=requirements,
+    install_requires=["requests", "pydub"],
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
