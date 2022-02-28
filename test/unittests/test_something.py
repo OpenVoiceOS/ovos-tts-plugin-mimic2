e04d7b46f6e9784f56ec05455b311f6a2d7fb751
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-02-28 22:11:49 +0000

    Feat/voice selection (#6)
    * add nancy voice
    
    * add ljspeech voice
    
    * add voice support
    
    authored-by: jarbasai <jarbasai@mailfence.com>

--- test/unittests/test_something.py
+++ test/unittests/test_something.py
@@ -5,13 +5,10 @@ from ovos_tts_plugin_mimic2 import Mimic2TTSPlugin
 
 
 class TestTTS(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.mimic = Mimic2TTSPlugin()
-
-    def test_something(self):
+    def test_kusal(self):
         path = "/tmp/hello_kusal.wav"
-        audio, phonemes = self.mimic.get_tts("hello world", path)
+        mimic = Mimic2TTSPlugin()
+        audio, phonemes = mimic.get_tts("hello world", path)
         self.assertEqual(audio, path)
         self.assertEqual(phonemes,
                          [['HH', '0.0775'],
@@ -22,3 +19,17 @@ class TestTTS(unittest.TestCase):
                           ['ER', '0.5580'],
                           ['L', '0.6820'],
                           ['D', '0.8060']])
+
+    def test_nancy(self):
+        path = "/tmp/hello_nancy.wav"
+        mimic = Mimic2TTSPlugin(config={"voice": "nancy"})
+        audio, phonemes = mimic.get_tts("hello world", path)
+        self.assertEqual(audio, path)
+        self.assertEqual(phonemes, None)
+
+    def test_ljspeech(self):
+        path = "/tmp/hello_ljspeech.wav"
+        mimic = Mimic2TTSPlugin(config={"voice": "ljspeech"})
+        audio, phonemes = mimic.get_tts("hello world", path)
+        self.assertEqual(audio, path)
+        self.assertEqual(phonemes, None)
